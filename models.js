95b1cef0b889f4069430d54a7d034f5198e890a0
June Hosoya <junehosoya@gmail.com>
2019-01-18 12:53:14 -0500

    seed.js file force true complete

--- models.js
+++ models.js
@@ -1,13 +1,12 @@
-const sq = require('sequelize');
-const db = new sq('postgres://localhost:5432/plantr');
+const sq = require("sequelize");
+const db = new sq("postgres://localhost:5432/plantr");
+//.define => we are defining the table model. VERY IMPORTANT
+const plantrTable = db.define("plantr");
 
-const plantrTable = db.define('plantr')
-
-
-.then(() => {
-    db.sync({force: true});
-}) .catch (err => {
-    db.close();
-})
+// .then(() => {
+//     db.sync({force: true});
+// }) .catch (err => {
+//     db.close();
+// })
 
 module.exports = db;
--- seed.js
+++ seed.js
@@ -1,3 +1,17 @@
-const model = require('./models')
+//FS code:
+const db = require("./models");
 
-module.exports = {};
+db.sync({ force: true })
+  .then(() => {
+    console.log("Database synced!");
+    // db.close() // only if using a version of node without `finally`
+  })
+  .catch(err => {
+    console.log("Disaster! Something went wrong! ");
+    console.log(err);
+    // db.close() // only if using a version of node without `finally`
+  })
+  .finally(() => {
+    // only if using a version of node WITH `finally`
+    db.close();
+  });
